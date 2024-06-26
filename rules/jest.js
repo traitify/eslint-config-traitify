3430d798025d21265ab6e29396db28fcacec4b34
Tomify <tprats108@gmail.com>
2024-06-26 14:09:23 -0400

    Added jest config

---
+++ rules/jest.js
@@ -0,0 +1,7 @@
+module.exports = {
+  rules: {
+    "import/no-unresolved": "off", // Enable when jest import/resolver is fixed
+    "no-new": "off", // Tests are exempt
+    "react/prop-types": "off" // Tests are exempt
+  }
+};
