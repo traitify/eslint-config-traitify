942b94fa0ab94ff48201b77daba96e54c49221a1
Tomify <tprats108@gmail.com>
2025-10-02 16:46:47 -0400

    Fixed jest

--- config/jest.js
+++ config/jest.js
@@ -4,20 +4,11 @@ import rulesJest from "../rules/jest.js";
 
 export default defineConfig([
   {
-    languageOptions: {
-      globals: {
-        ...pluginJest.environments.globals.globals,
-        container: "writable"
-      }
-    }
-  },
-  {
     ...pluginJest.configs["flat/recommended"],
     extends: [
       ...(pluginJest.configs["flat/recommended"].extends || []),
       rulesJest
     ],
-    files: ["**/*.spec.js", "**/*.test.js"],
     languageOptions: {
       ...pluginJest.configs["flat/recommended"].languageOptions,
       globals: {
